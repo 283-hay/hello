629071440be035e9ab5f551ebe534cb2828a8e97
Tsubasa Hayakawa <0wy5703254k9g@gmail.com>
2025-01-25 19:44:32 +0900

    4th commit

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import pandas  as pd
 
 st.title('Streamlit 超入門')
 
-with open('requirements.txt', 'r', encoding='utf-8') as file:
+with open('db1.txt', 'r', encoding='utf-8') as file:
     # ファイルの内容を読み込む
     content = file.read()
 
 st.title(content)
 
 # 追記モードでファイルを開く
-with open('requirements.txt', 'a', encoding='utf-8') as file:
+with open('db1.txt', 'a', encoding='utf-8') as file:
     # 追記する内容
     file.write('\n新しい内容を追記します。')
