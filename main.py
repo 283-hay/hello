3f242245e96bec54e54b30df82efbf0a2b5d7e30
Tsubasa Hayakawa <0wy5703254k9g@gmail.com>
2025-01-25 19:27:05 +0900

    2nd commit

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import numpy as np
 import pandas  as pd
 
 st.title('Streamlit 超入門')
+
+with open('requirements.txt', 'r', encoding='utf-8') as file:
+    # ファイルの内容を読み込む
+    content = file.read()
+
+# ファイルの内容を表示する
+print(content)
+
+st.title(content)
